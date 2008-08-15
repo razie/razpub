6d2bc6128e4c1c6c1a411852acdd6810c5f348ca
razie <razie@razie>
2008-08-15 18:22:52 +0000

    yada yada yada

--- src/com/razie/pub/SuitePub.java
+++ src/com/razie/pub/SuitePub.java
@@ -6,7 +6,9 @@ package com.razie.pub;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import com.razie.pub.base.test.SuiteBase;
 import com.razie.pub.draw.test.SuiteDraw;
+import com.razie.pub.events.test.SuiteEvents;
 import com.razie.pub.http.test.SuiteHttp;
 import com.razie.pub.lightsoa.test.SuiteLightsoa;
 
@@ -19,10 +21,11 @@ public class SuitePub extends TestSuite {
     public static Test suite() {
         TestSuite result = new TestSuite(SuitePub.class.getName());
 
+        result.addTest(SuiteBase.suite());
+        result.addTest(SuiteEvents.suite());
         result.addTest(SuiteDraw.suite());
         result.addTest(SuiteHttp.suite());
         result.addTest(SuiteLightsoa.suite());
-
 
         return result;
     }
