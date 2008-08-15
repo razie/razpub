459d48b742f3ddc4cbfcf59f1518ce3f32d24c72
razie <razie@razie>
2008-08-15 18:18:54 +0000

    yada yada yada

--- src/com/razie/pubstage/agent/AgentGroup.java
+++ src/com/razie/pubstage/agent/AgentGroup.java
@@ -1,5 +1,9 @@
+/**
+ * Razvan's code. Copyright 2008 based on Apache (share alike) see LICENSE.txt for details.
+ */
 package com.razie.pubstage.agent;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,5 +28,30 @@ import com.razie.pub.http.AgentHandle;
  */
 public class AgentGroup {
     /** map<name,handle> */
-    public Map<String, AgentHandle> agents = new HashMap<String, AgentHandle>();
+    private Map<String, AgentHandle> agents    = Collections
+                                                      .synchronizedMap(new HashMap<String, AgentHandle>());
+
+    // TODO implement this
+    public static AgentGroup        homeGroup = null;
+
+    /**
+     * only access is to clone the sync'd collection. The individual agents may still be modified by
+     * async status updates, but as assignments are atomic should be ok
+     */
+    public Map<String, AgentHandle> agents() {
+        synchronized (agents) {
+            Map<String, AgentHandle> copy = new HashMap<String, AgentHandle>();
+            copy.putAll(agents);
+            return copy;
+        }
+    }
+
+    public AgentHandle put (String name, AgentHandle h) {
+        agents.put(name, h);
+        return h;
+    }
+    
+    public AgentHandle get (String name) {
+        return agents.get(name);
+    }
 }
