fd2a21263327e9aa1a035eb83a7b4fb66461b900
razie <razie@razie>
2008-09-26 16:57:39 +0000

    moved stuff around...

---
+++ src/com/razie/pubstage/UserPrefs.java
@@ -0,0 +1,75 @@
+//==========================================================================
+// $Id: Svc.java,v 1.63 2005/04/01 16:22:12 davidx Exp $
+// (@) Copyright Sigma Systems (Canada) Inc.
+//==========================================================================
+package com.razie.pubstage;
+
+import com.razie.pub.base.AttrAccess;
+
+/**
+ * store user preferences
+ * 
+ * TODO create/maintain file
+ * 
+ * TODO sync file across agents
+ * 
+ * @version $Revision: 1.63 $
+ * @author $Author: davidx $
+ * @since $Date: 2005/04/01 16:22:12 $
+ */
+public class UserPrefs {
+
+    public static enum AssetListVisual {
+        BRIEFLIST, LIST, DETAILS
+    }
+
+    private static UserPrefs singleton;
+
+    public static UserPrefs getInstance() {
+        if (singleton == null) {
+            singleton = new UserPrefs();
+
+            for (String[] pair : defaults) {
+                singleton.setPref(pair[0], pair[1]);
+            }
+
+            // TODO load from file and overwrite defaults
+        }
+        return singleton;
+    }
+
+    /** preferences are cascaded by default a.b.c defaults to a.b defaults to a */
+    public String getPref(String name, String dflt) {
+        String parent = name;
+        String val = (String) this.parms.getAttr(name);
+
+        while (val == null) {
+            parent = parent.replaceFirst("\\..*$", "");
+            if (parent == null || parent.length() <= 0 || parent.equals(name)) {
+                break;
+            }
+
+            val = (String) this.parms.getAttr(parent);
+        }
+
+        return val == null ? dflt : val;
+    }
+
+    public boolean hasPref(String name) {
+        return this.parms.isPopulated(name);
+    }
+
+    public void setPref(String name, String value) {
+        this.parms.setAttr(name, value);
+    }
+
+    AttrAccess              parms        = new AttrAccess.Impl();
+
+    static final String[][] defaults     = { { "listvisual", AssetListVisual.LIST.toString() },
+            { "listvisual.Movie", AssetListVisual.BRIEFLIST.toString() },
+            { "listvisual.Site", AssetListVisual.LIST.toString() },
+            { "listvisual.Series", AssetListVisual.DETAILS.toString() } };
+
+    // TODO reflect configuration from here...
+    static final String[][] defaultTypes = { { "listvisual", AssetListVisual.class.getName() } };
+}
