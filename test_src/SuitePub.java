0afaa5464c61831e489775655d208a636d3cc008
razie <razie@razie>
2009-02-21 16:10:00 +0000

    moved stuff to razbase

---
+++ test_src/SuitePub.java
@@ -0,0 +1,35 @@
+/**
+ * Razvan's code. Copyright 2008 based on Apache (share alike) see LICENSE.txt for details.
+ */
+
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import com.razie.pub.agent.test.SuiteAgents;
+import com.razie.pub.base.test.SuiteBaseTest;
+import com.razie.pub.draw.test.SuiteDraw;
+import com.razie.pub.events.test.SuiteEvents;
+import com.razie.pub.http.test.SuiteHttp;
+import com.razie.pub.lightsoa.test.SuiteLightsoa;
+
+/**
+ * suite to run all pub tests
+ * 
+ * @author razvanc99
+ */
+public class SuitePub extends TestSuite {
+    public static Test suite() {
+        TestSuite result = new TestSuite(SuitePub.class.getName());
+
+        result.addTest(SuiteBaseTest.suite());
+        result.addTest(SuiteEvents.suite());
+        result.addTest(SuiteDraw.suite());
+        result.addTest(SuiteHttp.suite());
+        result.addTest(SuiteLightsoa.suite());
+        result.addTest(SuiteAgents.suite());
+
+        return result;
+    }
+
+}
