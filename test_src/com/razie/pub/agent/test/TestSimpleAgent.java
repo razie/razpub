dbb22c8aec9630839c6e76785a751cd5c9374bc0
razie <razie@razie>
2009-08-10 19:15:20 +0000

    sync-ing small changes

--- test_src/com/razie/pub/agent/test/TestSimpleAgent.java
+++ test_src/com/razie/pub/agent/test/TestSimpleAgent.java
@@ -7,17 +7,14 @@ package com.razie.pub.agent.test;
 import junit.framework.TestCase;
 
 import com.razie.pub.agent.Agent;
-import com.razie.pub.agent.AgentHttpService;
+import com.razie.pub.agent.TempUtilAgent;
 import com.razie.pub.base.ActionItem;
-import com.razie.pub.base.ThreadContext;
 import com.razie.pub.base.log.Log;
 import com.razie.pub.comms.ActionToInvoke;
 import com.razie.pub.comms.AgentCloud;
 import com.razie.pub.comms.AgentHandle;
 import com.razie.pub.comms.LightAuth;
 import com.razie.pub.comms.ServiceActionToInvoke;
-import com.razie.pub.http.LightCmdGET;
-import com.razie.pub.http.LightServer;
 
 /**
  * sample for a simple agent
@@ -25,59 +22,42 @@ import com.razie.pub.http.LightServer;
  * @author razvanc99
  */
 public class TestSimpleAgent extends TestCase {
-	static AgentHandle me = new AgentHandle("testagent1", "localhost",
-			"127.0.0.1", "4446", "http://localhost:4446");
-	static AgentHandle other = new AgentHandle("testagent2", "localhost",
-			"127.0.0.1", "4447", "http://localhost:4447");
 
-	public void setUp() {
-		LightAuth.init(new LightAuth("lightsoa"));
-	}
+   static AgentHandle me = TempUtilAgent.me;
+   static AgentHandle other = TempUtilAgent.other;
 
-	/** start an agent and mount basic services */
-	public static Agent startAgent(AgentHandle h, AgentCloud g) {
-		Agent agent = new Agent(h, g);
-		agent.getMainContext().enter();
-		agent.onInit();
+   @Override
+   public void setUp() {
+      LightAuth.init(new LightAuth("lightsoa"));
+   }
 
-		LightAuth.init(new LightAuth("lightsoa"));
+   /** start an agent and mount basic services */
+   public static Agent startAgent(AgentHandle h, AgentCloud g) {
+      // TODO inline
+      return TempUtilAgent.startAgent(h, g);
+   }
 
-		// we need a server with cmdget to accept bindings for services. You
-		// should do this for any
-		// agent
-		LightServer server = new LightServer(Integer.parseInt(h.port), agent
-				.getMainContext());
-		server.registerCmdListener(new LightCmdGET());
-		agent.register(new AgentHttpService(agent, server));
+   // create an agent, do something with it and destroy it
+   public void testDumbAgent() throws InterruptedException {
+      AgentCloud group = new AgentCloud();
+      group.put(me);
+      Agent agent = startAgent(me, group);
 
-		agent.onStartup();
-        ThreadContext.exit();
-		return agent;
-	}
+      try {
+         SampleJavaService sampleService = new SampleJavaService();
+         agent.register(sampleService);
+         Thread.sleep(500);
 
-	// create an agent, do something with it and destroy it
-	public void testDumbAgent() throws InterruptedException {
-		AgentCloud group = new AgentCloud();
-		group.put(me);
-		Agent agent = startAgent(me, group);
+         ActionToInvoke a = new ServiceActionToInvoke(me.url, "samplejavaservice",
+                 new ActionItem("echo2"));
+         Object oo = a.act(null);
+         Log.logThis("REPLY: " + oo.toString());
+         assertTrue(oo.toString().contains("echo2"));
+      } finally {
+         agent.onShutdown();
+      }
 
-		try {
-			SampleJavaService sampleService= new SampleJavaService();
-			agent.register(sampleService);
-			Thread.sleep(500);
-
-			ActionToInvoke a = new ServiceActionToInvoke(me.url, "samplejavaservice",
-					new ActionItem("echo2"));
-			Object oo = a.act(null);
-			Log.logThis("REPLY: " + oo.toString());
-			assertTrue(oo.toString().contains("echo2"));
-		} finally {
-			agent.onShutdown();
-		}
-
-		Thread.sleep(500);
-	}
-
-	static final Log logger = Log.Factory.create(TestSimpleAgent.class
-			.getName());
+      Thread.sleep(500);
+   }
+   static final Log logger = Log.Factory.create(TestSimpleAgent.class.getName());
 }
