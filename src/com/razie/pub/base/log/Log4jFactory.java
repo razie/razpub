fdbd1d22e59ac5e598e5c39e12fc640a7b4388cd
razvanc <razvanc@razubuntu.(none)>
2010-08-07 21:34:53 -0400

    all kinda of changes

---
+++ src/com/razie/pub/base/log/Log4jFactory.java
@@ -0,0 +1,20 @@
+package com.razie.pub.base.log;
+
+/**
+ * This is how you can use any underlying logging package: simply overwrite this with your own
+ * factory before the thing starts (first thing in main())
+ * 
+ * OR 
+ * 
+ * Just overwrite this in the classpath - easier, eh?
+ */
+public class Log4jFactory extends Factory {
+
+   public Log create(String component, String categoryName) {
+      return new Log4j(component, categoryName);
+   }
+
+   public Log create(String categoryName) {
+      return new Log4j("?", categoryName);
+   }
+}
